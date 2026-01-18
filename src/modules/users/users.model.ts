0d900bffdb993535cef372fcf4f6cbc571b5f87d
Muh-Uzair <muhammaduzair1062001@gmail.com>
2026-01-18 15:07:16 +0500

    user model done

---
+++ src/modules/users/users.model.ts
@@ -0,0 +1,69 @@
+import { Document, model, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  fullName: string;
+  email: string;
+  password: string;
+  role: string;
+  isEmailVerified: boolean;
+  otp: string;
+  otpExpiry: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters long"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin", "moderator"],
+      default: "user",
+      lowercase: true,
+    },
+
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    otp: {
+      type: String,
+      default: null,
+    },
+
+    otpExpiry: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const UserModel = model<IUser>("User", userSchema);
+
+export default UserModel;
